c68ba3c260ea0e0f65c8015af984d3a5e27d9d4a
Alexander Merzlikin <daikihanma@outlook.com>
2022-12-20 18:42:03 +0300

    fix: signin signup

--- src/components/Signup/Signup.tsx
+++ src/components/Signup/Signup.tsx
@@ -1,4 +1,8 @@
-import { SignBadge } from '../index';
+import { useState } from 'react';
+
+import { SignBadge } from '../';
+import { api } from '../../utils/api';
+
 import './Signup.css';
 
 interface SignupProps {
@@ -6,6 +10,32 @@ interface SignupProps {
 }
 
 const Signup: React.FC<SignupProps> = ({guestEntrance}) => {
+  const [email, setEmail] = useState<string>('');
+  const [name, setName] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+
+  const onChangeEmail = (event: React.FormEvent<HTMLInputElement>): void => {
+    setEmail(event.currentTarget.value);
+  };
+
+  const onChangeName = (event: React.FormEvent<HTMLInputElement>): void => {
+    setName(event.currentTarget.value);
+  };
+
+  const onChangePasswod = (event: React.FormEvent<HTMLInputElement>): void => {
+    setPassword(event.currentTarget.value);
+  };
+
+  const submit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    
+    api
+      .signUp(name, email, password)
+      .then(message => console.log(message))
+      .catch(error => console.log(error));
+  }
+  
+
   return(
     <main className='signup'>
       <div className='signup__container'>
@@ -15,16 +45,16 @@ const Signup: React.FC<SignupProps> = ({guestEntrance}) => {
           linkText='вход' 
           link='/signin'
         />
-        <form className='signup__form'>
+        <form className='signup__form' onSubmit={submit}>
           <h2 className='signup__title'>Регистрация</h2>
           <div className='signup__input-field'>
-            <input className='signup__input' placeholder='Имя пользователя' />
+            <input value={email} onChange={onChangeEmail} className='signup__input' placeholder='Имя пользователя' />
           </div>
           <div className='signup__input-field'>
-            <input className='signup__input' placeholder='Почта' />
+            <input value={name} onChange={onChangeName} className='signup__input' placeholder='Почта' type='email' />
           </div>
           <div className='signup__input-field'>
-            <input className='signup__input' placeholder='Пароль' />
+            <input value={password} onChange={onChangePasswod} className='signup__input' placeholder='Пароль' type='password' />
           </div>
 
           <button className='signup__submit' type='submit'>Зарегистрироваться</button>
