a15b06e95624608ba5dc5c83647d6d052a216c0d
Alexander Merzlikin <daikihanma@outlook.com>
2023-01-01 16:14:53 +0300

    fix: links

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -8,19 +8,19 @@ const Footer: React.FC = () => {
         <h2 className='footer__contacts-title'>Контакты</h2>
         <ul className='footer__contacts-list'>
           <li className='footer__item'>
-            <a href='/' className='footer__link'>
+            <a href='/' className='footer__link' target='_blank' rel='noreferrer'>
               <div className='footer__icon footer__icon_be'></div>
               Designer
             </a>
           </li>
           <li className='footer__item'>
-            <a href='/' className='footer__link'>
+            <a href='https://github.com/HanmaDaiki' className='footer__link' target='_blank' rel='noreferrer'>
               <div className='footer__icon footer__icon_git'></div>
               Frontend Developer
             </a>
           </li>
           <li className='footer__item'>
-            <a href='/' className='footer__link'>
+            <a href='https://github.com/VisioNaryy' className='footer__link' target='_blank' rel='noreferrer'>
               <div className='footer__icon footer__icon_git'></div>
               Backend Developer
             </a>
