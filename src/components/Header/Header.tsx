93729ac888f50f99cbca09f5b6956f9edcaec7ae
Alexander Merzlikin <daikihanma@outlook.com>
2023-01-01 16:15:07 +0300

    add: Profile links

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,3 +1,4 @@
+import { NavLink } from 'react-router-dom';
 import { Logo } from '../';
 import './Header.css';
 
@@ -5,6 +6,10 @@ const Header: React.FC = () => {
   return(
     <header className='header'>
       <Logo />
+      <NavLink className='profile-link' to='/profile'>
+        Профиль
+        <div className='profile-link__icon'></div>
+      </NavLink>
     </header>
   );
 };
